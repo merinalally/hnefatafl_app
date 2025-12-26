a092bb17e76b5889ede854ca8bdd62598cef9998
merinalally <merina.latbi@icloud.com>
2025-12-26 11:20:46 +0100

    Refactorization Start and Menu Screen
    Refactorization Start and Menu Screen. Use ScreenWidget

--- lib/screen/menu_screen.dart
+++ lib/screen/menu_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tafl_app/widget/option_button.dart';
 import 'package:tafl_app/provider/game_screen_provider.dart';
+import 'package:tafl_app/widget/screen_widget.dart';
 import 'package:tafl_app/widget/title_widget.dart';
 
 
@@ -46,28 +47,10 @@ class MenuScreen extends ConsumerWidget {
       ),
     );
 
-    Widget widget = Stack(
-      children: [
-        Positioned.fill(
-          child: Image.asset(
-            'assets/images/screenV.png',
-            fit: BoxFit.cover,
-          ),
-        ),
-        Positioned.fill(
-          child: Container(
-            color: Colors.black.withValues(alpha: 0.5),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: content,
-            ),
-          ),
-        ),
-      ],
-    );
-
     return Scaffold(
-      body: widget,
+      body: ScreenWidget.river(
+        content: content,
+      ),
     );
 
   }
